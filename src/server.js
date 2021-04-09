96fa4f20858b4bd73dd434553fd72d79abf21201
Luis Cetta <luiscetta@gmail.com>
2021-04-09 13:36:51 -0300

    creating-static-routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const server = express()
 
+// Rota (antes de entrar no GET - Habilitar arquivos statics)
+server.use(express.static("public"))
+
 // request, response 
 server.get('/', (request, response) => {
-    return response.send("OI!!!")
+    return response.sendFile(__dirname + "/views/index.html")
 })
 
 
-server.listen(3000, () => console.log('rodando'))
+server.listen(3000, () => console.log('rodando')) 
