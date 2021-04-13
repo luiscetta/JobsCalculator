e60223fdee9b624440009b1ec698272ce5fea335
Luis Cetta <luiscetta@gmail.com>
2021-04-13 00:07:56 -0300

    creating-post-route/ authorization-req.body

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const  views = __dirname + "/views/"
 // criando perfil estático
 const profile = {
     name: "Luis Cetta",
-    avatar: "https://avatars.githubusercontent.com/u/74614495?v=4",
+    avatar: "https://github.com/luiscetta.png",
     'monthly-budget': 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
@@ -17,6 +17,12 @@ const profile = {
 // req, res 
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
+
+// criar/salvar dados job  
+routes.post('/job', (req, res) => {
+    console.log(req.body)
+})
+
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
