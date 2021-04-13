495133dd7ca488aec17a7d17a24fe3310720c8b3
Luis Cetta <luiscetta@gmail.com>
2021-04-13 17:08:14 -0300

    refactoring-routes-code-remaining-days

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const routes = express.Router();
 
-const  views = __dirname + "/views/"
+const views = __dirname + "/views/"
 
 // criando perfil estático
 const profile = {
@@ -21,19 +21,40 @@ const jobs = [
         name: "Take Burguer Hamburgueria",
         'daily-hours': 2,
         'total-hours': 60,
-        createdAt: Date.now() 
+        createdAt: Date.now(),
     },
     {
         id: 2,
         name: "Caldinhos da Nanda",
         'daily-hours': 5,
         'total-hours': 37,
-        createdAt: Date.now() 
+        createdAt: Date.now(),
     },
 ];
 
 // req, res 
-routes.get('/', (req, res) => res.render(views + "index", { jobs }))
+routes.get('/', (req, res) => {
+
+    // Ajustes no Jobs - Calculo de tempo restante
+    const updatedJobs = jobs.map((job) => {
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed() // => toFixed: se o numero for quebrado, ele deixa inteiro.
+
+        // Dia de criação do job
+        const createdDate = new Date(job.createdAt)
+
+        // Dia do vencimento do job
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+
+        // Data exata do vencimento.
+        // const dueDate = createdDate.setDate
+
+
+        return job
+
+    })
+
+    return res.render(views + "index", { jobs })
+})
 routes.get('/job', (req, res) => res.render(views + "job"))
 
 // criar/salvar dados job  
@@ -42,7 +63,7 @@ routes.post('/job', (req, res) => {
     // job.createdAt = new Date.now();
 
     // criando ID para os jobs. Conforme vai adicionando jobs, o Id vai mudando automáticamente conforme a posição no array.
-    const lastId = jobs[jobs.length - 1]?.id || 1;  
+    const lastId = jobs[jobs.length - 1]?.id || 1;
 
     // jogando os dados para o array jobs
     jobs.push({
@@ -63,4 +84,4 @@ routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 
 // export routes
-module.exports = routes; 
+module.exports = routes;
